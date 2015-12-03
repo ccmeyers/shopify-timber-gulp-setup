b2d4fd7d1820f352e4aaed0474cfb0c26624dfd4
ccmeyers <ccmeyers@gmail.com>
2015-12-03 18:43:37 -0500

    adds gulp sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 // Gulp plugin setup
-var gulp = require('gulp');
+var gulp         = require('gulp');
 // Watches single files
-var watch = require('gulp-watch');
-var gulpShopify = require('gulp-shopify-upload');
+var watch        = require('gulp-watch');
+var gulpShopify  = require('gulp-shopify-upload');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var neat         = require('node-neat').includePaths;
 // Grabs your API credentials
-var config = require('config.json');
+var config       = require('config.json');
  
 gulp.task('shopifywatch', function() {
   var options = {
@@ -14,8 +17,18 @@ gulp.task('shopifywatch', function() {
   return watch('./Timber/+(assets|layout|config|snippets|templates|locales)/**')
   .pipe(gulpShopify(config.shopify_api_key, config.shopify_api_password, config.shopify_url, null, options));
 });
+
+gulp.task('sass', function () {
+  gulp.src('./lib/scss/*.{sass,scss}')
+    .pipe(sass({includePaths: neat}))
+    .pipe(gulp.dest('./Timber/assets/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./lib/scss/**/*.{sass,scss}', ['sass']);
+});
  
 // Default gulp action when gulp is run
 gulp.task('default', [
-  'shopifywatch'
+  'shopifywatch', 'sass:watch'
 ]);
