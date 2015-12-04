1aecc41547cc3856a38fb5cd4c1b3b1e5ad12224
ccmeyers <ccmeyers@gmail.com>
2015-12-04 16:15:53 -0500

    adds browserify gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,34 @@
 var gulp         = require('gulp');
 // Watches single files
 var watch        = require('gulp-watch');
+// Uploads changes to Shopify
 var gulpShopify  = require('gulp-shopify-upload');
+// Compiles SCSS files
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+// Notifies of errors
+var notify = require("gulp-notify");
+// Includes the Bourbon Neat libraries
 var neat         = require('node-neat').includePaths;
 // Grabs your API credentials
 var config       = require('config.json');
+// Concats your JS files
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+
+  // Send error to notification center with gulp-notify
+  notify.onError({
+    title: "Compile Error",
+    message: "<%= error %>"
+  }).apply(this, args);
+
+  // Keep gulp from hanging on this task
+  this.emit('end');
+}
  
 gulp.task('shopifywatch', function() {
   var options = {
@@ -21,14 +43,38 @@ gulp.task('shopifywatch', function() {
 gulp.task('sass', function () {
   gulp.src('./lib/scss/*.{sass,scss}')
     .pipe(sass({includePaths: neat}))
+    .on('error', handleErrors)
+    .pipe(autoprefixer({ browsers: ['last 2 version'] }))
     .pipe(gulp.dest('./Timber/assets/'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./lib/scss/**/*.{sass,scss}', ['sass']);
+gulp.task('browserify', function() {
+  return browserify('./lib/js/app.js')
+      .bundle()
+      .on('error', handleErrors)
+      //Pass desired output filename to vinyl-source-stream
+      .pipe(source('bundle.js'))
+      // Start piping stream to tasks!
+      .pipe(gulp.dest('./Timber/assets/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./lib/scss/**/*.scss', ['sass']);
+  gulp.watch('./lib/js/**/*.js', ['browserify']);
+
+  var watcher = watchify(browserify({
+    // Specify the entry point of your app
+    entries: ['./lib/js/app.js'],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function() {
+    watcher.bundle()
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./Timber/assets/'))
+  })
 });
  
 // Default gulp action when gulp is run
-gulp.task('default', [
-  'shopifywatch', 'sass:watch'
-]);
+gulp.task('default', ['shopifywatch', 'watch']);
